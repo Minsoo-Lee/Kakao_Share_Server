0157c954b2018c8eb1272dc6a55c0b78ac5196f3
이민수 <minsoo.james1101@gmail.com>
2025-05-16 03:21:55 +0900

    interval: 2 min -> 1 hour

--- automation/automator.py
+++ automation/automator.py
@@ -31,7 +31,7 @@ def start_task():
     scheduler.add_job(
         lambda: threading.Thread(target=safe_task, daemon=False).start(),
         'interval',
-        minutes=2
+        hours=1
     )
     scheduler.start()
 
