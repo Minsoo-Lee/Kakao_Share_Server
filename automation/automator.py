1c40e5dcd779aeac9c295abde42671531367b56b
이민수 <minsoo.james1101@gmail.com>
2025-05-17 01:14:04 +0900

    change to hours 2

--- automation/automator.py
+++ automation/automator.py
@@ -31,7 +31,7 @@ def start_task():
     scheduler.add_job(
         lambda: threading.Thread(target=safe_task, daemon=False).start(),
         'interval',
-        minutes=60
+        hours=2
     )
 
     scheduler.start()
