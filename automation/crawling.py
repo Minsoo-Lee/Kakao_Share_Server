e69942dc05c0602f80af6eb25efaf95c3cac1653
이민수 <minsoo.james1101@gmail.com>
2025-05-22 20:36:09 +0900

    GPT 버전 분명히 바꿨는데..??

--- automation/crawling.py
+++ automation/crawling.py
@@ -157,8 +157,11 @@ def crawl_lists_title():
         article_list = []
         article_list.append(from_naver(NAVER_IT_URL))
         article_list.append(from_naver(NAVER_SOCIAL_URL))
-        article_list.append(from_iboss())
+        # article_list.append(from_iboss())
         article_list.append(from_google())
+
+        for i in range(len(article_list)):
+            print(article_list[i][0])
 
         # 뒤에도 보게 하도록 리스트를 랜덤하게 셔플
         # print("기사를 랜덤하게 셔플합니다.")
@@ -216,11 +219,13 @@ def from_naver(url):
     # return [[title_list[i].text, href_list[i]] for i in range(LIST_LEN)]
 
     article_list = [[title_list[i].text, href_list[i]] for i in range(len(title_list))]
+    for i in range(len(article_list)):
+        print(article_list[i][0])
     index = get_one(article_list)
 
     # 출력결과
-    print("title = " + article_list[index][0])
-    print("title = " + article_list[index][1])
+    # print("title = " + article_list[index][0])
+    # print("title = " + article_list[index][1])
     return article_list[index]
 
 def from_iboss():
@@ -244,10 +249,12 @@ def from_iboss():
     time.sleep(2)
     # return [[title_list[i], href_list[i]] for i in range(min(20, len(title_list), len(href_list)))]
     article_list = [[title_list[i], href_list[i]] for i in range(len(title_list))]
+    for i in range(len(article_list)):
+        print(article_list[i][0])
     index = get_one(article_list)
 
-    print("title = " + article_list[index][0])
-    print("title = " + article_list[index][1])
+    # print("title = " + article_list[index][0])
+    # print("title = " + article_list[index][1])
     return article_list[index]
 
 def from_google():
@@ -271,6 +278,7 @@ def from_google():
 
     url_lists = soup.find_all("a", class_=lambda x: x and 'WlydOe' in x)
     href_list = [li['href'] for li in url_lists if li.has_attr('href')]
+    print(title_list)
 
     # for title in title_list:
     #     print(title)
@@ -281,8 +289,9 @@ def from_google():
     # return [[title_list[i], href_list[i]] for i in range(min(20, len(title_list), len(href_list)))]
     article_list = [[title_list[i], href_list[i]] for i in range(len(title_list))]
     index = get_one(article_list)
-    print("title = " + article_list[index][0])
-    print("title = " + article_list[index][1])
+
+    # print("title = " + article_list[index][0])
+    # print("title = " + article_list[index][1])
     return article_list[index]
 
 def get_one(article_list):
